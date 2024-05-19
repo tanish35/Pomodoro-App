2e54de12b6c87c7963947f85d68df27e884b9ad5
tanish35 <tanishmajumdar2912@gmail.com>
2024-05-19 11:24:15 +0530

    setting up google oauth2.0

---
+++ google/auth.js
@@ -0,0 +1,18 @@
+import passport from "passport";
+import { Strategy as GoogleStrategy } from "passport-google-oauth20";
+import "dotenv/config";
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: "http://localhost:5000/auth/google/callback",
+    },
+    function (accessToken, refreshToken, profile, cb) {
+      User.findOrCreate({ googleId: profile.id }, function (err, user) {
+        return cb(err, user);
+      });
+    }
+  )
+);
