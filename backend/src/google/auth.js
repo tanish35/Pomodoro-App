83ed4e5609b296531486dfff91c37a3f4b2733ae
tanish35 <tanishmajumdar2912@gmail.com>
2024-05-20 12:59:32 +0530

    Minor change

--- backend/src/google/auth.js
+++ backend/src/google/auth.js
@@ -7,7 +7,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:3000/auth/google/callback",
+      callbackURL: `http://localhost:${process.env.PORT}/auth/google/callback`,
     },
     async (accessToken, refreshToken, profile, cb) => {
       try {
