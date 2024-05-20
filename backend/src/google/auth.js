fa5f1d58f4bb2bcfd085f0face934fe348c42e82
tanish35 <tanishmajumdar2912@gmail.com>
2024-05-20 12:21:57 +0530

    Change ports

--- backend/src/google/auth.js
+++ backend/src/google/auth.js
@@ -20,7 +20,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:5000/auth/google/callback",
+      callbackURL: "http://localhost:3000/auth/google/callback",
     },
     async (accessToken, refreshToken, profile, cb) => {
       try {
