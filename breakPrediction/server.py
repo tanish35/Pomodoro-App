831905ec4a28edec029a889e286de59c406673c2
tanish35 <tanishmajumdar2912@gmail.com>
2024-05-22 11:51:06 +0530

    Increased accuracy

--- breakPrediction/server.py
+++ breakPrediction/server.py
@@ -21,6 +21,6 @@ def predict_endpoint(request: PredictRequest):
             period="Day"
 
         prediction1 = prediction.predict_breaks(period, request.totalHours, request.age)
-        return {"break": prediction1*request.totalHours,"period":period}
+        return {"break": int(prediction1*request.totalHours),"period":period}
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
