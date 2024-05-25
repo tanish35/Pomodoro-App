7632eb9fb30e9ea9e314da776321962bc8db42b1
tanish35 <142332924+tanish35@users.noreply.github.com>
2024-05-25 12:51:02 +0530

    Update checkAuth.js

--- backend/src/middleware/checkAuth.js
+++ backend/src/middleware/checkAuth.js
@@ -1,8 +1,13 @@
 import jwt from "jsonwebtoken";
 import prisma from "../lib/prisma.js";
+
 async function requireAuth(req, res, next) {
   try {
-    const token = req.cookies.Authorization;
+    const token = req.session.token; // Retrieve JWT token from session
+    if (!token) {
+      res.sendStatus(401);
+      return;
+    }
     const decoded = jwt.verify(token, process.env.SECRET);
     if (Date.now() >= decoded.exp) {
       res.sendStatus(410);
