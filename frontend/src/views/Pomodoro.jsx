e9f024d949f90f395974badd2de2b3c289b6ee8f
tanish35 <tanishmajumdar2912@gmail.com>
2024-05-25 13:30:06 +0530

    Minor change

--- frontend/src/views/Pomodoro.jsx
+++ frontend/src/views/Pomodoro.jsx
@@ -1,4 +1,4 @@
-import Timer from "../components/timer";
+import Timer from "../components/Timer.jsx";
 import Header from "../components/Header";
 import { useState } from "react";
 import Setting from "../components/Setting";
@@ -6,23 +6,35 @@ import { TimerProvider } from "../components/TimerContext";
 import backgroundImage from "../assets/background-1.jpg";
 
 export default function TimerPomodoro() {
-    const [isSettingOpen, setIsSettingOpen] = useState(false)
+  const [isSettingOpen, setIsSettingOpen] = useState(false);
 
-    const handleSettingClick = () => {
-        setIsSettingOpen(!isSettingOpen);
-    }
+  const handleSettingClick = () => {
+    setIsSettingOpen(!isSettingOpen);
+  };
 
-    const handleCloseSetting = () =>{
-        setIsSettingOpen(false)
-    }
-
+  const handleCloseSetting = () => {
+    setIsSettingOpen(false);
+  };
 
   return (
     <TimerProvider>
-<div style={{ backgroundImage: `url(${backgroundImage})`, backgroundSize: 'cover', backgroundPosition: 'center', backgroundRepeat: 'no-repeat', width: '100vw', height: '100vh', position: 'relative', zIndex: '98' }}>       <Header onSettingClick={handleSettingClick} ></Header>
-      <Timer></Timer>
-      {isSettingOpen && <Setting onClose={handleCloseSetting} />}
-    </div>
+      <div
+        style={{
+          backgroundImage: `url(${backgroundImage})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+          width: "100vw",
+          height: "100vh",
+          position: "relative",
+          zIndex: "98",
+        }}
+      >
+        {" "}
+        <Header onSettingClick={handleSettingClick}></Header>
+        <Timer></Timer>
+        {isSettingOpen && <Setting onClose={handleCloseSetting} />}
+      </div>
     </TimerProvider>
   );
 }
