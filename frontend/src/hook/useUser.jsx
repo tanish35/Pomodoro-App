468f1bf1ca09f3f551fe98ca18e6e6708b27f12f
tanish35 <tanishmajumdar2912@gmail.com>
2024-05-26 22:35:27 +0530

    Fixed / route

--- frontend/src/hook/useUser.jsx
+++ frontend/src/hook/useUser.jsx
@@ -1,6 +1,5 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { BACKEND_URL } from "../config";
 
 export const useUser = () => {
   const [loading, setLoading] = useState(true);
@@ -9,31 +8,37 @@ export const useUser = () => {
   const [history, setHistory] = useState([]);
 
   async function getUserData() {
-    const res = await axios.get(`/api/user/me`, {
-      withCredentials: true,
-    });
-    setUserData(res.data);
-    if (!res.data.username) {
+    try {
+      const res = await axios.get(`/api/user/me`, {
+        withCredentials: true,
+      });
+      setUserData(res.data);
+
+      const stats = await axios.get(`/api/stats/${res.data.username}`, {
+        withCredentials: true,
+      });
+      if (!stats.data) {
+        setStats([]);
+        setLoading(false);
+      }
+      setStats(stats.data);
+
+      const history = await axios.get(
+        `/api/stats/history/${res.data.username}`,
+        {
+          withCredentials: true,
+        }
+      );
+      if (!history.data) {
+        setLoading(false);
+      }
+      setHistory(history.data);
+      setLoading(false);
+    } catch (e) {
+      setUserData([]);
       setLoading(false);
       return;
     }
-    const stats = await axios.get(`/api/stats/${res.data.username}`, {
-      withCredentials: true,
-    });
-    if (!stats.data) {
-      setStats([]);
-      setLoading(false);
-    }
-    setStats(stats.data);
-
-    const history = await axios.get(`/api/stats/history/${res.data.username}`, {
-      withCredentials: true,
-    });
-    if (!history.data) {
-      setLoading(false);
-    }
-    setHistory(history.data);
-    setLoading(false);
   }
 
   useEffect(() => {
--- frontend/src/pages/Index/IndexPage.jsx
+++ frontend/src/pages/Index/IndexPage.jsx
@@ -1,15 +1,14 @@
-import React from 'react'
-import { useUser } from '../../hook/useUser'
-import { Navigate } from 'react-router-dom';
-import './IndexPage.css'
+import React from "react";
+import { useUser } from "../../hook/useUser";
+import { Navigate } from "react-router-dom";
+import "./IndexPage.css";
 const IndexPage = () => {
-  const {loading, userData} = useUser();
-  
+  const { loading, userData } = useUser();
+
   if (loading) {
     return (
       <div class="preloader">
         <div class="preloader__ring">
-
           <div class="preloader__sector"></div>
           <div class="preloader__sector"></div>
           <div class="preloader__sector"></div>
@@ -26,18 +25,18 @@ const IndexPage = () => {
           <div class="preloader__sector"></div>
           <div class="preloader__sector"></div>
         </div>
-      <div class="preloader__text">Loading...</div>
-      <div class="preloader__bar"></div>
-    </div>
-    )
+        <div class="preloader__text">Loading...</div>
+        <div class="preloader__bar"></div>
+      </div>
+    );
   }
-  if (!userData) {
-    return <Navigate to='/signin' />
+  if (userData.length == 0) {
+    return <Navigate to="/signin" />;
   }
 
   if (userData) {
-    return <Navigate to='/profile/dashboard' />
+    return <Navigate to="/profile/dashboard" />;
   }
-}
+};
 
-export default IndexPage
+export default IndexPage;
