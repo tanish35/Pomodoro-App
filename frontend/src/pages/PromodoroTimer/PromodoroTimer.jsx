b2dd7f4d627d5a9168c4b7d3a964bba0bf37fd42
tanish35 <tanishmajumdar2912@gmail.com>
2024-05-31 14:17:04 +0530

    Updated Spotify input

--- frontend/src/pages/PromodoroTimer/PromodoroTimer.jsx
+++ frontend/src/pages/PromodoroTimer/PromodoroTimer.jsx
@@ -21,7 +21,9 @@ const Pomodoro = () => {
   const [startMinutes, setStartMinutes] = useState(0);
   const [dataPosting, setDataPosting] = useState(false);
   const [inputLink, setInputLink] = useState("");
-  const [playListLink, setPlayListLink] = useState("https://open.spotify.com/playlist/37i9dQZF1DZ06evNZVVBPG?si=8bc8cebd99fb492f");
+  const [playListLink, setPlayListLink] = useState(
+    "https://open.spotify.com/playlist/37i9dQZF1DZ06evNZVVBPG?si=8bc8cebd99fb492f"
+  );
   const intervalRef = useRef(null);
   const [task, setTask] = useState("");
   const { loading, userData } = useUser();
@@ -168,7 +170,6 @@ const Pomodoro = () => {
     return formatDateTime(futureDate);
   }
 
-
   useEffect(() => {
     if (!isRunning) {
       setTimeLeft(sessionLength * 60);
@@ -185,22 +186,28 @@ const Pomodoro = () => {
     >
       <Sidebar />
       <div className="bg-transparent p-6 rounded-lg w-full max-w-lg mx-auto text-center">
-      
-      <div className={`${css.spotify}`}>
-      <div className="flex items-center justify-center p-5">
-  <div className="rounded-lg pr-5">
-    <div className="flex">
-      <div className="flex w-16 items-center justify-center rounded-tl-lg rounded-bl-lg border-r border-gray-200 bg-white p-5">
-        <svg viewBox="0 0 20 20" aria-hidden="true" class="pointer-events-none absolute w-5 fill-gray-500 transition">
-          <path d="M16.72 17.78a.75.75 0 1 0 1.06-1.06l-1.06 1.06ZM9 14.5A5.5 5.5 0 0 1 3.5 9H2a7 7 0 0 0 7 7v-1.5ZM3.5 9A5.5 5.5 0 0 1 9 3.5V2a7 7 0 0 0-7 7h1.5ZM9 3.5A5.5 5.5 0 0 1 14.5 9H16a7 7 0 0 0-7-7v1.5Zm3.89 10.45 3.83 3.83 1.06-1.06-3.83-3.83-1.06 1.06ZM14.5 9a5.48 5.48 0 0 1-1.61 3.89l1.06 1.06A6.98 6.98 0 0 0 16 9h-1.5Zm-1.61 3.89A5.48 5.48 0 0 1 9 14.5V16a6.98 6.98 0 0 0 4.95-2.05l-1.06-1.06Z"></path>
-        </svg>
-      </div>
-      <input type="text" value={inputLink} onChange={(e) => setInputLink(e.target.value)} className="w-full max-w-[160px] bg-white pl-2 text-base text-black font-semibold outline-0" placeholder="" id="" />
-      <input type="button" value="Search" className="bg-blue-500 p-2 rounded-tr-lg rounded-br-lg text-white font-semibold hover:bg-blue-800 transition-colors" onClick={(e) => setPlayListLink(inputLink)} />
-    </div>
-  </div>
-</div>
-        <SpotifyComponent playListLink={playListLink} />
+        <div className={`${css.spotify}`}>
+          <div className="flex items-center justify-center p-5">
+            <div className="rounded-lg pr-5">
+              <div className="flex">
+                <input
+                  type="text"
+                  value={inputLink}
+                  onChange={(e) => setInputLink(e.target.value)}
+                  className="w-64 bg-white pl-2 text-base text-black font-semibold outline-0 rounded-lg"
+                  placeholder="Spotify playlist/track link"
+                  id=""
+                />
+                <input
+                  type="button"
+                  value="Play"
+                  className="bg-blue-500 p-2 rounded-tr-lg rounded-br-lg text-white font-semibold hover:bg-blue-800 transition-colors"
+                  onClick={(e) => setPlayListLink(inputLink)}
+                />
+              </div>
+            </div>
+          </div>
+          <SpotifyComponent playListLink={playListLink} />
         </div>
         {/* <Spotify link={playListLink} className="spotify-embed w-full lg:w-1/2 rounded-xl shadow-lg" /> */}
 
