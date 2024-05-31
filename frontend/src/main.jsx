a9976280f7f4a74bd14fbcffb92f37e470c738d1
tanish35 <tanishmajumdar2912@gmail.com>
2024-05-31 14:23:31 +0530

    Minor change

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App.jsx";
 import "./index.css";
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:3000/";
+axios.defaults.baseURL = "https://api.tanish.me";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
