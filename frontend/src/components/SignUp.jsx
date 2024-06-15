8b2dd6026c8cd8f6d1142b9207c57427418a629d
tanish35 <tanishmajumdar2912@gmail.com>
2024-06-15 09:38:56 +0530

    Added email regex in signup

--- frontend/src/components/SignUp.jsx
+++ frontend/src/components/SignUp.jsx
@@ -40,6 +40,12 @@ const Signup = () => {
       return;
     }
 
+    const emailRegex = /^[\w-]+(\.[\w-]+)*@gmail\.com$/;
+    if (!emailRegex.test(form.email)) {
+      alert("Email must be a Gmail address. This is being done to combat spam");
+      return;
+    }
+
     setLoading(true);
     try {
       await axios
@@ -60,7 +66,9 @@ const Signup = () => {
   };
 
   return (
-    <div className={`xl:mt-12 flex xl:flex-row flex-col-reverse gap-10 overflow-hidden`}>
+    <div
+      className={`xl:mt-12 flex xl:flex-row flex-col-reverse gap-10 overflow-hidden`}
+    >
       <motion.div
         variants={slideIn("left", "tween", 0.2, 1)}
         className="flex-[0.75] bg-black-100 p-8 rounded-2xl h-full"
@@ -84,31 +92,29 @@ const Signup = () => {
             />
           </label>
           <div className="flex flex-row">
-          <label className="flex flex-col w-[380px]">
-            <span className="text-white font-medium mb-4">Name</span>
-            <input
-              type="text"
-              name="name"
-              value={form.name}
-              onChange={handleChange}
-              placeholder="Enter your Name"
-              className="bg-tertiary py-4 px-6 placeholder:text-secondary text-black rounded-lg outline-none border-none font-medium"
-            />
-          </label>
-          <div className="invisible">
-          &nbsp
-          </div>
-          <label className="flex flex-col w-[90px]">
-            <span className="text-white font-medium mb-4">Age</span>
-            <input
-              type="number"
-              name="age"
-              value={form.age}
-              onChange={handleChange}
-              placeholder="Age"
-              className="bg-tertiary py-4 px-6 placeholder:text-secondary text-black rounded-lg outline-none border-none font-medium"
-            />
-          </label>
+            <label className="flex flex-col w-[380px]">
+              <span className="text-white font-medium mb-4">Name</span>
+              <input
+                type="text"
+                name="name"
+                value={form.name}
+                onChange={handleChange}
+                placeholder="Enter your Name"
+                className="bg-tertiary py-4 px-6 placeholder:text-secondary text-black rounded-lg outline-none border-none font-medium"
+              />
+            </label>
+            <div className="invisible">&nbsp</div>
+            <label className="flex flex-col w-[90px]">
+              <span className="text-white font-medium mb-4">Age</span>
+              <input
+                type="number"
+                name="age"
+                value={form.age}
+                onChange={handleChange}
+                placeholder="Age"
+                className="bg-tertiary py-4 px-6 placeholder:text-secondary text-black rounded-lg outline-none border-none font-medium"
+              />
+            </label>
           </div>
           <label className="flex flex-col">
             <span className="text-white font-medium mb-4">Email</span>
@@ -121,7 +127,7 @@ const Signup = () => {
               className="bg-tertiary py-4 px-6 placeholder:text-secondary text-black rounded-lg outline-none border-none font-medium"
             />
           </label>
-          
+
           <label className="flex flex-col">
             <span className="text-white font-medium mb-4">Password</span>
             <input
@@ -134,10 +140,7 @@ const Signup = () => {
             />
           </label>
 
-          <Link
-            to="/signin"
-            className="flex items-center gap-2 text-white"
-          >
+          <Link to="/signin" className="flex items-center gap-2 text-white">
             Already have an account?
             <div className="text-cyan-200">Login</div>
           </Link>
